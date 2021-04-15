6f5ec466fc7ae5660cf5a78db25012e2663a9e0c
Amelie119 <79218475+Amelie119@users.noreply.github.com>
2021-04-15 16:50:04 +0200

    Add files via upload

--- recept.php
+++ recept.php
@@ -4,4 +4,24 @@ echo "Je suis la page de réception des données" ;
 var_dump($_POST) ;
 var_dump($_FILES) ;
 echo $_FILES['PhotoPresentation'] ;
+
+require_once("../../initCore.php");
+
+
+
+
+$extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
+
+$extension_upload = strtolower(  substr(  strrchr($_FILES['PhotoPresentation']['name'], '.')  ,1)  );
+
+$cheminImageSurServeur = DIR_SRV."images/".$_FILES['PhotoPresentation']['name'];
+$resultat = move_uploaded_file($_FILES['PhotoPresentation']['tmp_name'],$cheminImageSurServeur);
+
+$urlServeur = "/images/".$_FILES['PhotoPresentation']['name'];
+
+echo $resultat;
+
 ?>
+
+
+
