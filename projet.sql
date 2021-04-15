b43319d4cd6c07e0d599526cdf9fac73d4e14726
Amelie119 <79218475+Amelie119@users.noreply.github.com>
2021-04-15 16:47:07 +0200

    Add files via upload

---
+++ projet.sql
@@ -0,0 +1,259 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 15 avr. 2021 à 14:10
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `abonnement`
+--
+
+DROP TABLE IF EXISTS `abonnement`;
+CREATE TABLE IF NOT EXISTS `abonnement` (
+  `idabonnement` int(11) NOT NULL AUTO_INCREMENT,
+  `prix` float NOT NULL,
+  `nomabonnement` varchar(100) NOT NULL,
+  PRIMARY KEY (`idabonnement`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaire`
+--
+
+DROP TABLE IF EXISTS `commentaire`;
+CREATE TABLE IF NOT EXISTS `commentaire` (
+  `idcommentaire` int(11) NOT NULL AUTO_INCREMENT,
+  `dateCommentaire` date NOT NULL,
+  `contenucom` text NOT NULL,
+  `idutilisateur` int(11) NOT NULL,
+  `idtuto` int(11) NOT NULL,
+  PRIMARY KEY (`idcommentaire`),
+  KEY `Commentaire_Utilisateur_FK` (`idutilisateur`),
+  KEY `Commentaire_Tuto0_FK` (`idtuto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `donner_acces`
+--
+
+DROP TABLE IF EXISTS `donner_acces`;
+CREATE TABLE IF NOT EXISTS `donner_acces` (
+  `idtuto` int(11) NOT NULL,
+  `idabonnement` int(11) NOT NULL,
+  PRIMARY KEY (`idtuto`,`idabonnement`),
+  KEY `Donner_acces_Abonnement0_FK` (`idabonnement`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etape`
+--
+
+DROP TABLE IF EXISTS `etape`;
+CREATE TABLE IF NOT EXISTS `etape` (
+  `idetape` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(100) NOT NULL,
+  `texte` text NOT NULL,
+  `idtuto` int(11) NOT NULL,
+  `idphoto` int(11) NOT NULL,
+  PRIMARY KEY (`idetape`),
+  KEY `etape_tuto_FK` (`idtuto`),
+  KEY `etape_photo_FK` (`idphoto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `materiel`
+--
+
+DROP TABLE IF EXISTS `materiel`;
+CREATE TABLE IF NOT EXISTS `materiel` (
+  `idmateriel` int(11) NOT NULL AUTO_INCREMENT,
+  `nommateriel` varchar(50) NOT NULL,
+  PRIMARY KEY (`idmateriel`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `necessite`
+--
+
+DROP TABLE IF EXISTS `necessite`;
+CREATE TABLE IF NOT EXISTS `necessite` (
+  `idmateriel` int(11) NOT NULL,
+  `idtuto` int(11) NOT NULL,
+  PRIMARY KEY (`idmateriel`,`idtuto`),
+  KEY `necessite_Tuto0_FK` (`idtuto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photo`
+--
+
+DROP TABLE IF EXISTS `photo`;
+CREATE TABLE IF NOT EXISTS `photo` (
+  `idphoto` int(11) NOT NULL AUTO_INCREMENT,
+  `datePhoto` date NOT NULL,
+  `titrePhoto` text NOT NULL,
+  `chemin` varchar(100) NOT NULL,
+  `idtuto` int(11) DEFAULT NULL,
+  `idcommentaire` int(11) DEFAULT NULL,
+  `idetape` int(11) NOT NULL,
+  PRIMARY KEY (`idphoto`),
+  KEY `Photo_Tuto_FK` (`idtuto`),
+  KEY `Photo_Commentaire1_FK` (`idcommentaire`),
+  KEY `Photo_etape_FK` (`idetape`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `souscrire`
+--
+
+DROP TABLE IF EXISTS `souscrire`;
+CREATE TABLE IF NOT EXISTS `souscrire` (
+  `idabonnement` int(11) NOT NULL,
+  `idutilisateur` int(11) NOT NULL,
+  `typePaiement` varchar(50) NOT NULL,
+  `dateDebut` date NOT NULL,
+  `dateresiliation` date NOT NULL,
+  PRIMARY KEY (`idabonnement`,`idutilisateur`),
+  KEY `Souscrire_Utilisateur0_FK` (`idutilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `suivre`
+--
+
+DROP TABLE IF EXISTS `suivre`;
+CREATE TABLE IF NOT EXISTS `suivre` (
+  `idtuto` int(11) NOT NULL,
+  `idutilisateur` int(11) NOT NULL,
+  PRIMARY KEY (`idtuto`,`idutilisateur`),
+  KEY `Suivre_Utilisateur0_FK` (`idutilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tuto`
+--
+
+DROP TABLE IF EXISTS `tuto`;
+CREATE TABLE IF NOT EXISTS `tuto` (
+  `idtuto` int(11) NOT NULL AUTO_INCREMENT,
+  `dateCreation` date NOT NULL,
+  `theme` varchar(255) NOT NULL,
+  `titreTuto` text NOT NULL,
+  `contenuetuto` text NOT NULL,
+  `textpresentation` text NOT NULL,
+  PRIMARY KEY (`idtuto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `idutilisateur` int(11) NOT NULL AUTO_INCREMENT,
+  `themeFavori` varchar(80) NOT NULL,
+  `nom` varchar(80) NOT NULL,
+  `prenom` varchar(80) NOT NULL,
+  `dateNaissance` date NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mdp` varchar(100) NOT NULL,
+  PRIMARY KEY (`idutilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD CONSTRAINT `Commentaire_Tuto0_FK` FOREIGN KEY (`idtuto`) REFERENCES `tuto` (`idtuto`),
+  ADD CONSTRAINT `Commentaire_Utilisateur_FK` FOREIGN KEY (`idutilisateur`) REFERENCES `utilisateur` (`idutilisateur`);
+
+--
+-- Contraintes pour la table `donner_acces`
+--
+ALTER TABLE `donner_acces`
+  ADD CONSTRAINT `Donner_acces_Abonnement0_FK` FOREIGN KEY (`idabonnement`) REFERENCES `abonnement` (`idabonnement`),
+  ADD CONSTRAINT `Donner_acces_Tuto_FK` FOREIGN KEY (`idtuto`) REFERENCES `tuto` (`idtuto`);
+
+--
+-- Contraintes pour la table `etape`
+--
+ALTER TABLE `etape`
+  ADD CONSTRAINT `etape_photo_FK` FOREIGN KEY (`idphoto`) REFERENCES `photo` (`idphoto`),
+  ADD CONSTRAINT `etape_tuto_FK` FOREIGN KEY (`idtuto`) REFERENCES `tuto` (`idtuto`);
+
+--
+-- Contraintes pour la table `necessite`
+--
+ALTER TABLE `necessite`
+  ADD CONSTRAINT `necessite_Tuto0_FK` FOREIGN KEY (`idtuto`) REFERENCES `tuto` (`idtuto`),
+  ADD CONSTRAINT `necessite_materiel_FK` FOREIGN KEY (`idmateriel`) REFERENCES `materiel` (`idmateriel`);
+
+--
+-- Contraintes pour la table `photo`
+--
+ALTER TABLE `photo`
+  ADD CONSTRAINT `Photo_Commentaire1_FK` FOREIGN KEY (`idcommentaire`) REFERENCES `commentaire` (`idcommentaire`),
+  ADD CONSTRAINT `Photo_Tuto_FK` FOREIGN KEY (`idtuto`) REFERENCES `tuto` (`idtuto`),
+  ADD CONSTRAINT `Photo_etape_FK` FOREIGN KEY (`idetape`) REFERENCES `etape` (`idetape`);
+
+--
+-- Contraintes pour la table `souscrire`
+--
+ALTER TABLE `souscrire`
+  ADD CONSTRAINT `Souscrire_Abonnement_FK` FOREIGN KEY (`idabonnement`) REFERENCES `abonnement` (`idabonnement`),
+  ADD CONSTRAINT `Souscrire_Utilisateur0_FK` FOREIGN KEY (`idutilisateur`) REFERENCES `utilisateur` (`idutilisateur`);
+
+--
+-- Contraintes pour la table `suivre`
+--
+ALTER TABLE `suivre`
+  ADD CONSTRAINT `Suivre_Tuto_FK` FOREIGN KEY (`idtuto`) REFERENCES `tuto` (`idtuto`),
+  ADD CONSTRAINT `Suivre_Utilisateur0_FK` FOREIGN KEY (`idutilisateur`) REFERENCES `utilisateur` (`idutilisateur`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
