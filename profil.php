bc43b8f0a97ffa1c7f725d0373b5ba33817d4950
Amelie119 <79218475+Amelie119@users.noreply.github.com>
2021-04-18 14:09:09 +0200

    Add files via upload

---
+++ profil.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=projet', 'root', '');
+
+if(isset($_GET['id']) AND $_GET['id'] > 0) {
+   $getid = intval($_GET['id']);
+   $requser = $bdd->prepare('SELECT * FROM utilisateur WHERE idutilisateur = ?');
+   $requser->execute(array($getid));
+   $userinfo = $requser->fetch();
+?>
+<html>
+   <head>
+      <title>TUTO PHP</title>
+      <meta charset="utf-8">
+   </head>
+   <body>
+      <div align="center">
+         <h2>Profil de <?php echo $userinfo['pseudo']; ?></h2>
+         <br /><br />
+         Pseudo = <?php echo $userinfo['pseudo']; ?>
+         <br />
+         Mail = <?php echo $userinfo['email']; ?>
+         <br />
+         <?php
+         if(isset($_SESSION['id']) AND $userinfo['idutilisateur'] == $_SESSION['id']) {
+         ?>
+         <br />
+         <a href="deconnection.php">Se déconnecter</a>
+         <?php
+         }
+         ?>
+      </div>
+   </body>
+</html>
+<?php   
+}
+?>
