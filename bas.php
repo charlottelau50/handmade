2e6748e8ffca93d3193fbe20396c3d2c29735792
charlottelau50 <78498073+charlottelau50@users.noreply.github.com>
2021-04-21 15:24:39 +0200

    Add files via upload

---
+++ bas.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Homemade</title>
+		<meta charset="UTF-8">
+		<link href="style-3.css" rel="stylesheet">
+	</head>
+    <body>
+      <div id="couleur-3-bas-page">
+			<div id="bas-page">
+				<div class="colonne-1">
+					<p><a  href="projet.html">Accueil</a></p>
+                    <p><a href="forum.html">A votre tour!</a></p>
+				</div>
+				<div class="colonne-2">
+                <p><a href="couture.html">Couture</a></p>
+					<p><a href="decoration.html">Décoration</a></p>
+					<p><a href="cuisine.html">Cuisine</a></p>
+                    <p><a href="cosmetique.html">Cosmétique</a></p>
+					<p><a href="prod-menagers.html">Produits ménagers</a></p>					
+				</div>
+				<div class="colonne-3">
+					<p><a  href="credit.html">Crédits</a></p>
+					<p><a href="inscription2.php">S'inscrire</a></p>
+					<p><a href="connexion.php">Se connecter</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="titre1">
+			<img class="titre-fin" src="image/titre-3.jpg" alt="titre"/>
+		</div>
+	</body>
+</html>
---
+++ fil.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Homemade</title>
+		<meta charset="UTF-8">
+		<link href="style-3.css" rel="stylesheet">
+	</head>
+
+	<body>
+	
+		<div class="titre1">
+			<img class="titre" src="image/titre-3.jpg" alt="titre"/>
+		</div>
+	
+		<div class="fil">
+				<div class="categorie">
+					<a  href="projet.php">Accueil</a>
+				</div>
+				<div class="categorie"> 
+					<a href="couture.html">Couture</a>
+					<div class="diy">
+						<p class="lien"><a href="couvercle.html">Couvercle</a></p>
+						<p class="lien"><a href="chouchou.html">Chouchou</a></p>
+						<p class="lien"><a href="coton.html">Coton</a></p>
+						<p class="lien"><a href="sac.html">Sac</a></p>
+					</div>
+				</div>
+				<div class="categorie"> 
+					<a href="decoration.html">Décoration</a>
+					<div class="diy">
+						<p class="lien"><a href="toile.html" >Toile</a></p>
+						<p class="lien"> <a href="calendrier.html"> Semainier</a></p>
+						<p class="lien"><a href="Bougie.html">Bougie</a></p>
+					</div>		
+				</div>
+				<div class="categorie"> 
+					<a href="cuisine.html">Cuisine</a>
+					<div class="diy">
+						<p class="lien"><a href="compote.html" >Compote</a></p>
+					</div>		
+				</div>
+				<div class="categorie">
+					<a  href="cosmetique.html">Cosmétique</a>
+				</div>
+				<div class="categorie">
+					<a  href="prod-menagers.html">Produits Ménagers</a>
+				</div>
+				<div class="categorie">
+					<a  href="forum.html">A votre tour!</a>
+				</div>
+				<div class="categorie">
+					<a  href="connexion.html">Connexion</a>
+				</div>
+		</div>
+    </body>
+</html>
