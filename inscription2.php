461485dc7e7e68598b70ddce2b5435effd781d1c
Anaisjal28 <78499162+Anaisjal28@users.noreply.github.com>
2021-04-26 16:24:44 +0200

    Update inscription2.php

--- inscription2.php
+++ inscription2.php
@@ -92,33 +92,9 @@
     ?>
 
 
-		<div class="titre1">
-			<img class="titre" src="image/titre-3.jpg" alt="titre"/>
-		</div>
-	
-		<div class="fil">
-				<div class="categorie">
-					<a  href="projet.html">Accueil</a>
-				</div>
-				<div class="categorie"> 
-					<a href="couture.html">Couture</a>
-					<div class="diy">
-						<p class="lien"><a href="couvercle.html">Couvercle</a></p>
-						<p class="lien"><a href="chouchou.html">Chouchou</a></p>
-					</div>
-				</div>
-				<div class="categorie"> 
-					<a href="decoration.html">Décoration</a>
-					<div class="diy">
-						<p class="lien"><a href="toile.html" >Toile</a></p>
-						<p class="lien"> <a href="calendrier.html"> Semainier</a></p>
-						<p class="lien"><a href="Bougie.html">Bougie</a></p>
-					</div>		
-				</div>
-				<div class="categorie">
-					<a  href="forum.html">A votre tour!</a>
-				</div>
-		</div>
+        <?php
+		include('fil.php');
+		?>
 		
 		<div class="acceuil">
 		<div class="titre-gd-h1">
@@ -235,25 +211,9 @@
 		</form>
 		</div>
 
-		<div id="couleur-3-bas-page">
-			<div id="bas-page">
-				<div class="colonne-1">
-					<p><a  href="projet.html">Accueil</a></p>
-					<p><a href="couture.html">Couture</a></p>
-					<p><a href="decoration">Décoration</a></p>
-					<p><a  href="forum.html">A votre tour!</a></p>
-				</div>
-				<div class="colonne-2">
-					<p><a href="inscription.html">S'inscrire</a></p>
-					<p><a href="connexion.html">Se connecter</a></p>
-				</div>
-				<div class="colonne-3">
-					<p><a  href="credit.html">Crédits</a></p>
-				</div>
-			</div>
-		</div>
-		<div class="titre1">
-			<img class="titre-fin" src="image/titre-3.jpg" alt="titre"/>
-		</div>
+		<?php
+		include('bas.php');
+		?>
+		
 	</body>
 </html>
