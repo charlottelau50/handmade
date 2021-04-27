81c4626ccd4a7c70492a9236a485490f8bfd85cd
Amelie119 <79218475+Amelie119@users.noreply.github.com>
2021-04-27 20:45:26 +0200

    Update commentaire.php

--- commentaire.php
+++ commentaire.php
@@ -35,8 +35,8 @@
                     $contenucom = (String) trim($contenucom);
 
                     if($valid){
-                        $req = $bdd->insert("INSERT INTO commentaire (note_com, dateCommentaire, contenucom, idutilisateur, idtuto) VALUES (?, ?, ?, ?, ?)", 
-                        $req->execute(array($note_com, $dateCommentaire, $contenucom, $_SESSION['idutilisateur'], $idtuto)));
+                        $req = $bdd->insert("INSERT INTO commentaire (note_com, dateCommentaire, contenucom, idutilisateur, idtuto) VALUES (?, ?, ?, ?, ?)"); 
+                        $req->execute(array($note_com, $dateCommentaire, $contenucom, $_SESSION['idutilisateur'], $idtuto));
                         header('Location: comment.php'); 
                         exit;
                     }
