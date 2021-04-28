79d44b9c7c8e11bb8ce5140d5aed23b4b06fdbce
Anaisjal28 <78499162+Anaisjal28@users.noreply.github.com>
2021-04-28 11:58:13 +0200

    Add files via upload

--- decoration.php
+++ decoration.php
@@ -8,48 +8,21 @@
 
 	<body>
 	
-	<div class="titre1">
-		<img class="titre" src="image/titre-3.jpg" alt="titre"/>
-	</div>
-	
-	<div class="fil">
-			<div class="categorie">
-				<a  href="projet.html">Accueil</a>
-			</div>
-			<div class="categorie"> 
-				<a href="couture.html">Couture</a>
-				<div class="diy">
-					<p class="lien"><a href="couvercle.html">Couvercle</a></p>
-					<p class="lien"><a href="chouchou.html">Chouchou</a></p>
-				</div>
-			</div>
-			<div class="categorie"> 
-				<a href="decoration.html">Décoration</a>
-				<div class="diy">
-					<p class="lien"><a href="toile.html" >Toile</a></p>
-					<p class="lien"><a href="calendrier.html" >Semainier</a></p>
-					<p class="lien"><a href="Bougie.html">Bougie</a></p>
-				</div>
-			</div>
-			<div class="categorie">
-				<a  href="forum.html">A votre tour!</a>
-			</div>
-		</div>
+		<?php
+            include('fil.php');
+        ?> 
 		
 		<div class="titre-gd-h1">
 			<h1>Décoration</h1>
 		</div>
 
-		
-		<div class="couture">
-			<img src="image/handmade.jpg" class="imgcategorie" alt="img-deco">
-		</div>
+	
 		
 		<div class="presentation-tuto">
 			<div class="liste-tuto">
 				<?php
 					include("fonctionBD.php");
-					$req=$bdd->query("SELECT tuto.idtuto, titreTuto, chemin FROM tuto, photo where theme='decoration' AND tuto.idtuto=photo.idtuto");
+					$req=$bdd->query("SELECT tuto.idtuto, titreTuto, chemin FROM tuto, photo where theme='couture' AND tuto.idtuto=photo.idtuto");
 					while($tuto=$req->fetch()){
 						$compteur=1;
 						$idtuto=$tuto['idtuto'];
@@ -70,27 +43,9 @@
 			</div>
 		</div>
 	
-	<div id="couleur-3-bas-page">
-		<div id="bas-page">
-			<div class="colonne-1">
-				<p><a  href="projet.html">Accueil</a></p>
-				<p><a href="couture.html">Couture</a></p>
-				<p><a href="decoration.html">Décoration</a></p>
-				<p><a  href="forum.html">A votre tour!</a></p>
-			</div>
-			<div class="colonne-2">
-				<p><a href="inscription.html">S'inscrire</a></p>
-				<p><a href="connexion.html">Se connecter</a></p>
-			</div>
-			<div class="colonne-3">
-				<p><a  href="credit.html">Crédits</a></p>
-			</div>
-		</div>
-	</div>
-	
-	<div class="titre1">
-		<img class="titre-fin" src="image/titre-3.jpg" alt="titre"/>
-	</div>
+		<?php
+            include('bas.php');
+        ?> 
 	
 		
 	</body>
